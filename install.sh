221e34aac7547598e7c40113a2860d3d84c1d3c0
70705 <127928758+70705@users.noreply.github.com>
2023-08-29 16:35:06 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ Include = /etc/pacman.d/chaotic-mirrorlist' | sudo tee -a /etc/pacman.conf
 sudo pacman -Syy qt5-quickcontrols2 qt5-graphicaleffects qt5-svg xdg-desktop-portal-xapp gvfs ffmpegthumbnailer tumbler thunar xorg-setxkbmap lsd ttf-jetbrains-mono ttf-jetbrains-mono-nerd ttf-terminus-nerd ttf-inconsolata ttf-joypixels papirus-icon-theme rofi dunst polybar xorg-xprop xorg-xkill physlock picom bspwm sxhkd xdg-user-dirs zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting xorg-xsetroot xorg-xwininfo xorg-xrandr polkit-gnome adwaita-cursors lxappearance pulsemixer libinput qalculate-gtk breeze-icons qt5ct spotify alacritty p7zip p7zip-gui sddm zip downgrade htop pinta ntfs-3g btrfs-progs cpupower yuzu-early-access grub efibootmgr firefox ttf-liberation ttf-dejavu noto-fonts noto-fonts-emoji noto-fonts-cjk inetutils nvidia-dkms connman discord mangohud lib32-mangohud mpv easyeffects steam qbittorrent calf zsh linux-headers nvidia-settings lib32-nvidia-utils nvidia-utils visual-studio-code-bin trackma-git git kvantum bottles gamemode lib32-gamemode heroic-games-launcher-git thunderbird xdg-user-dirs xdg-desktop-portal paru pipewire pipewire-pulse wireplumber
 
 mv paru/ $HOME/.config/
-paru -S alass unified-remote-server --noconfirm
+paru -S alass unified-remote-server sddm-theme-tokyo-night --noconfirm
 
 sudo systemctl enable sddm.service
 sudo systemctl enable polkit
@@ -53,7 +53,7 @@ nohup easyeffects --gapplication-service &
 /opt/urserver/urserver --daemon &' | sudo tee -a ~/.xprofile
 
 echo -e '[Theme]
-Current=tokyo-night-sddm-main' | sudo tee -a /etc/sddm.conf
+Current=tokyo-night-sddm' | sudo tee -a /etc/sddm.conf.d/themes.conf
 
 xdg-mime default thunar.desktop inode/directory
 
@@ -66,7 +66,6 @@ mv gtk-3.0/ $HOME/.config/
 mv gtk-4.0/ $HOME/.config/
 mv Thunar/ $HOME/.config/
 mv Kvantum/ $HOME/.config/
-sudo mv tokyo-night-sddm-main/ /usr/share/sddm/themes/
 sudo mv tokyo-night-grub/ /boot/grub/themes/
 mv .zshrc $HOME/
 
