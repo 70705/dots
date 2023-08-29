8bc3c4542a805e8bff29c0b40de413ae94cb3d6d
70705 <127928758+70705@users.noreply.github.com>
2023-08-29 17:09:23 -0300

    Add files via upload

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+
+
+
+alias mixer='pulsemixer'
+alias cls="clear"
+alias ls='lsd -a --group-directories-first'
+alias ll='lsd -la --group-directories-first'
---
+++ omf/init.fish
@@ -0,0 +1,2 @@
+set -g theme_display_rw no
+set -g theme_display_group no
